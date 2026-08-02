b375249839fecfccb047a74fb4e79de8eb548a18
b125116-byte <b125116@iiit-bh.ac.in>
2026-08-02 23:18:23 +0530

    Implement coin toss simulation for fair and biased coins

--- DAA lab 1/q2/q2.c
+++ DAA lab 1/q2/q2.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int main()
+{
+    int n, i;
+    int fairHeads = 0, biasedHeads = 0;
+
+    printf("Enter number of tosses: ");
+    scanf("%d", &n);
+
+    srand(time(NULL));
+
+    // Fair coin
+    for(i = 0; i < n; i++)
+    {
+        if(rand() % 2 == 1)
+            fairHeads++;
+    }
+
+    // Biased coin (70% Heads)
+    for(i = 0; i < n; i++)
+    {
+        if(rand() % 100 < 70)
+            biasedHeads++;
+    }
+
+    printf("\nFair Coin\n");
+    printf("Heads = %d\n", fairHeads);
+    printf("Probability = %.3f\n", (float)fairHeads/n);
+
+    printf("\nBiased Coin\n");
+    printf("Heads = %d\n", biasedHeads);
+    printf("Probability = %.3f\n", (float)biasedHeads/n);
+
+    return 0;
+}
